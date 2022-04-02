276f41355ca491769ae026f342c8793cfce5d7a0
arek <jack-swad@wp.pl>
2022-04-02 21:56:31 +0100

    pseudo data fetch with setTimeout simulation

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -5,12 +5,36 @@ import { Wrapper } from './UsersList.styles';
 
 class UsersList extends React.Component {
   state = {
-    users,
+    users: [],
   };
+
   deleteUser = (userName) => {
     const filteredUsers = this.state.users.filter(({ name }) => name !== userName);
     this.setState(() => ({ users: filteredUsers }));
   };
+
+  mockAPI = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (users) {
+          resolve([...users]);
+        } else {
+          reject(new Error(`Błont`));
+        }
+      }, 2000);
+    });
+  };
+
+  componentDidMount() {
+    this.mockAPI()
+      .then((users) => {
+        this.setState({ users: users });
+      })
+      .catch((err) => console.log(err));
+  }
+  componentDidUpdate() {}
+  componentWillUnmount() {}
+
   render() {
     const { users } = this.state;
     return (
