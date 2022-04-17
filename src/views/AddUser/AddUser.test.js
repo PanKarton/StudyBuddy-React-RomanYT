4b94c435ef6fe42e80d46aa7985bfb7d767f53d9
arek <jack-swad@wp.pl>
2022-04-17 13:46:42 +0100

    add user tests added

--- src/views/AddUser/AddUser.test.js
+++ src/views/AddUser/AddUser.test.js
@@ -17,8 +17,25 @@ describe('AddUser component', () => {
     fireEvent.change(screen.getByLabelText('Name'), { target: { value: 'Miriamcia' } });
     fireEvent.change(screen.getByLabelText('Attendance'), { target: { value: '100%' } });
     fireEvent.change(screen.getByLabelText('Average'), { target: { value: '6' } });
+    fireEvent.click(screen.getByLabelText('Terms consent'));
     fireEvent.click(screen.getByText('Add'));
 
-    screen.getByText('Maciej Łyżwa');
+    screen.getByText('Miriamcia');
+  });
+  it('Displays error when consent is not clicked', () => {
+    renderWithProviders(
+      <>
+        <AddUser />
+        <Dashboard />
+      </>,
+    );
+    fireEvent.change(screen.getByLabelText('Name'), { target: { value: 'Miriamcia' } });
+    fireEvent.change(screen.getByLabelText('Attendance'), { target: { value: '100%' } });
+    fireEvent.change(screen.getByLabelText('Average'), { target: { value: '6' } });
+    fireEvent.click(screen.getByText('Add'));
+
+    const newUser = screen.queryByText('Miriamcia');
+    expect(newUser).not.toBeInTheDocument();
+    screen.getByText('Cannot add student without terms consent');
   });
 });
