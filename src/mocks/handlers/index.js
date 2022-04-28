13fc932cde122865e0f6770d0e52aaec40468df9
arek <jack-swad@wp.pl>
2022-04-28 05:33:45 +0100

    downshift lib with tests works properly

--- src/mocks/handlers/index.js
+++ src/mocks/handlers/index.js
@@ -6,6 +6,7 @@ export const handlers = [
   rest.get('/groups', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ groups }));
   }),
+
   rest.get('/students/:groupID?', (req, res, ctx) => {
     if (req.params.groupID) {
       const filteredStudents = students.filter((student) => student.group === req.params.groupID);
@@ -13,8 +14,8 @@ export const handlers = [
     }
     return res(ctx.status(200), ctx.json({ students }));
   }),
+
   rest.post('/students/search', (req, res, ctx) => {
-    console.log(req.body.searchPhrase);
     const matchingStudents = req.body.searchPhrase ? students.filter((student) => student.name.toLowerCase().includes(req.body.searchPhrase.toLowerCase())) : [];
     return res(ctx.status(200), ctx.json({ students: matchingStudents }));
   }),
