c3537a71e7a8cb0de4290b7127fcaddff1aaf2e0
arek <jack-swad@wp.pl>
2022-04-28 19:46:28 +0100

    UserValueInput story created

---
+++ src/components/molecules/UserValueInput/UserValueInput.stories.js
@@ -0,0 +1,13 @@
+import UserValueInput from './UserValueInput';
+
+export default {
+  title: 'Components/Molecules/UserValueInput',
+  component: UserValueInput,
+};
+
+const Template = (args) => <UserValueInput {...args} name="test" id="test" />;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Insert Value',
+};
